7e59ca37acd0e2f645f627eadd3c45a841c8bee5
Teodor Dutu <teodor.dutu@gmail.com>
2019-11-06 22:45:22 +0200

    Adaugare schelet tema 2

---
+++ src/CommunicationChannel.java
@@ -0,0 +1,51 @@
+/**
+ * Class that implements the channel used by headquarters and space explorers to communicate.
+ */
+public class CommunicationChannel {
+
+	/**
+	 * Creates a {@code CommunicationChannel} object.
+	 */
+	public CommunicationChannel() {
+	}
+
+	/**
+	 * Puts a message on the space explorer channel (i.e., where space explorers write to and 
+	 * headquarters read from).
+	 * 
+	 * @param message
+	 *            message to be put on the channel
+	 */
+	public void putMessageSpaceExplorerChannel(Message message) {
+	}
+
+	/**
+	 * Gets a message from the space explorer channel (i.e., where space explorers write to and
+	 * headquarters read from).
+	 * 
+	 * @return message from the space explorer channel
+	 */
+	public Message getMessageSpaceExplorerChannel() {
+		return null;
+	}
+
+	/**
+	 * Puts a message on the headquarters channel (i.e., where headquarters write to and 
+	 * space explorers read from).
+	 * 
+	 * @param message
+	 *            message to be put on the channel
+	 */
+	public void putMessageHeadQuarterChannel(Message message) {
+	}
+
+	/**
+	 * Gets a message from the headquarters channel (i.e., where headquarters write to and
+	 * space explorer read from).
+	 * 
+	 * @return message from the header quarter channel
+	 */
+	public Message getMessageHeadQuarterChannel() {
+		return null;
+	}
+}
---
+++ src/SpaceExplorer.java
@@ -0,0 +1,76 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Set;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Class for a space explorer.
+ */
+public class SpaceExplorer extends Thread {
+
+	/**
+	 * Creates a {@code SpaceExplorer} object.
+	 * 
+	 * @param hashCount
+	 *            number of times that a space explorer repeats the hash operation
+	 *            when decoding
+	 * @param discovered
+	 *            set containing the IDs of the discovered solar systems
+	 * @param channel
+	 *            communication channel between the space explorers and the
+	 *            headquarters
+	 */
+	public SpaceExplorer(Integer hashCount, Set<Integer> discovered, CommunicationChannel channel) {
+	}
+
+	@Override
+	public void run() {
+	}
+	
+	/**
+	 * Applies a hash function to a string for a given number of times (i.e.,
+	 * decodes a frequency).
+	 * 
+	 * @param input
+	 *            string to he hashed multiple times
+	 * @param count
+	 *            number of times that the string is hashed
+	 * @return hashed string (i.e., decoded frequency)
+	 */
+	private String encryptMultipleTimes(String input, Integer count) {
+		String hashed = input;
+		for (int i = 0; i < count; ++i) {
+			hashed = encryptThisString(hashed);
+		}
+
+		return hashed;
+	}
+
+	/**
+	 * Applies a hash function to a string (to be used multiple times when decoding
+	 * a frequency).
+	 * 
+	 * @param input
+	 *            string to be hashed
+	 * @return hashed string
+	 */
+	private static String encryptThisString(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] messageDigest = md.digest(input.getBytes(StandardCharsets.UTF_8));
+
+			// convert to string
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < messageDigest.length; i++) {
+				String hex = Integer.toHexString(0xff & messageDigest[i]);
+				if (hex.length() == 1)
+					hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}

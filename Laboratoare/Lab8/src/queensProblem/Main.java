aae60f4363ad176e891d6f30b52f50f2e6bd9ef7
Teodor Dutu <teodor.dutu@gmail.com>
2019-11-15 15:21:04 +0200

    saru' mana, irina

--- Laboratoare/Lab8/src/queensProblem/Main.java
+++ Laboratoare/Lab8/src/queensProblem/Main.java
@@ -7,7 +7,7 @@ public class Main {
     static final int N = 10;
 
     public static void main(String[] args) {
-        ExecutorService tpe     = Executors.newFixedThreadPool(12);
+        ExecutorService tpe     = Executors.newFixedThreadPool(12);  // sarakilor
         int[] queenPositions    = new int[N];
 
         tpe.submit(new QueensPlacer(tpe, queenPositions, 0));

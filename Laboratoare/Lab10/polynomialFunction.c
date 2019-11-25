1f2bfc9768486dd4064acf2ec5545216e2465d40
Teodor Dutu <teodor.dutu@gmail.com>
2019-11-25 12:46:50 +0200

    [Lab 10] Calculare polinom folosind pipeline

--- Laboratoare/Lab10/polynomialFunction.c
+++ Laboratoare/Lab10/polynomialFunction.c
@@ -1,47 +1,113 @@
-#include<mpi.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MASTER      (0)
+#define MAX_X       (10)
 
 /**
  * @author cristian.chilipirea
  *
  */
+int main(int argc, char * argv[])
+{
+    int rank;
+    int nProcesses;
 
-int main(int argc, char * argv[]) {
-	int rank;
-	int nProcesses;
-	MPI_Init(&argc, &argv);
+    float coeff;
+    float* a;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
-	printf("Hello from %i/%i\n", rank, nProcesses);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
 
-	if(rank==0) { // This code is run by a single process
-		int polynomialSize;
-		int n;
-		int i;
-		int j;
-		float x;
+    float* v = malloc(3 * sizeof(*v));
 
-		FILE * polFunctionFile = fopen("a1.txt","rt");
-		fscanf(polFunctionFile, "%i", &polynomialSize);
-		float * a = malloc(sizeof(float)*polynomialSize);
-		for(i=0;i<polynomialSize;i++) {
-			fscanf(polFunctionFile, "%f", a+i);
-		}
-		fclose(polFunctionFile);
+    int nextProc = rank + 1, prevProc = rank - 1;
 
-		for(x=0; x<3; x+=1) {
-			float sum = 0;
-			float xPowI = 1;
-			for(i=0;i<polynomialSize;i++) {
-				sum += xPowI * a[i];
-				xPowI *= x; // more efficient than pow(x,i);
-			}
-			printf("%f ", sum);
-		}
-	}
+    // This code is run by a single process
+    if (rank == MASTER)
+    {
+        int i, polynomialSize;
+        float x;
 
-	MPI_Finalize();
-	return 0;
+        FILE* polFunctionFile = fopen("a2.txt", "rt");
+
+        fscanf(polFunctionFile, "%i", &polynomialSize);
+        a = malloc(polynomialSize * sizeof(*a));
+        
+        for (i = 0; i < polynomialSize; ++i)
+        {
+            fscanf(polFunctionFile, "%f", a + i);
+        }
+
+        fclose(polFunctionFile);
+
+        MPI_Scatter(
+            a,
+            1,
+            MPI_FLOAT,
+            &coeff,
+            1,
+            MPI_FLOAT,
+            MASTER,
+            MPI_COMM_WORLD
+        );
+
+        v[0] = a[MASTER];
+        v[2] = 1.f;
+
+        for (x = 0; x < MAX_X; ++x)
+        {
+            v[1] = x;
+
+            MPI_Send(v, 3, MPI_FLOAT, nextProc, MPI_TAG_UB, MPI_COMM_WORLD);
+        }
+
+        free(a);
+    } else
+    {
+        int i;
+
+        MPI_Scatter(
+            a,
+            1,
+            MPI_FLOAT,
+            &coeff,
+            1,
+            MPI_FLOAT,
+            MASTER,
+            MPI_COMM_WORLD
+        );
+
+        for (i = 0; i != MAX_X; ++i)
+        {
+            MPI_Recv(
+                v,
+                3,
+                MPI_FLOAT,
+                prevProc,
+                MPI_ANY_TAG,
+                MPI_COMM_WORLD,
+                MPI_STATUS_IGNORE
+            );
+
+            v[2] *= v[1];
+            v[0] += coeff * v[2];
+
+            if (nextProc == nProcesses)
+            {
+                printf("P(%f) = %f\n", v[1], v[0]);
+            } else
+            {
+                MPI_Send(v, 3, MPI_FLOAT, nextProc, MPI_TAG_UB, MPI_COMM_WORLD);
+            }
+        }
+    }
+
+    free(v);
+    
+    MPI_Finalize();
+
+    return 0;
 }

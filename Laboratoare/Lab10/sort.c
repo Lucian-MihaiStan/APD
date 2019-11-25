02a92f823441bcaad777808410fbe5d0fbb4166f
Teodor Dutu <teodor.dutu@gmail.com>
2019-11-25 13:48:25 +0200

    [Lab 10] Adaugare sortare cu pipeline

--- Laboratoare/Lab10/sort.c
+++ Laboratoare/Lab10/sort.c
@@ -1,90 +1,153 @@
-#include<mpi.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#define N 10000
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N           (100)
+#define MASTER      (0)
 
 /**
  * @author cristian.chilipirea
  *
  */
-
-
-void compareVectors(int * a, int * b) {
-	// DO NOT MODIFY
-	int i;
-	for(i = 0; i < N; i++) {
-		if(a[i]!=b[i]) {
-			printf("Sorted incorrectly\n");
-			return;
-		}
-	}
-	printf("Sorted correctly\n");
+void compareVectors(int* a, int* b)
+{
+    // DO NOT MODIFY
+    int i;
+    
+    for (i = 0; i < N; i++)
+    {
+        if (a[i] != b[i])
+        {
+            printf("Sorted incorrectly\n");
+            return;
+        }
+    }
+    printf("Sorted correctly\n");
 }
 
-void displayVector(int * v) {
-	// DO NOT MODIFY
-	int i;
-	int displayWidth = 2 + log10(v[N-1]);
-	for(i = 0; i < N; i++) {
-		printf("%*i", displayWidth, v[i]);
-	}
-	printf("\n");
+void displayVector(int* v)
+{
+    // DO NOT MODIFY
+    int i;
+    
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ", v[i]);
+    }
+
+    printf("\n");
 }
 
-int cmp(const void *a, const void *b) {
-	// DO NOT MODIFY
-	int A = *(int*)a;
-	int B = *(int*)b;
-	return A-B;
+int cmp(const void* a, const void* b)
+{
+    // DO NOT MODIFY
+    int A = *(int*)a;
+    int B = *(int*)b;
+    
+    return A - B;
 }
  
-int main(int argc, char * argv[]) {
-	int rank;
-	int nProcesses;
-	MPI_Init(&argc, &argv);
+int main(int argc, char* argv[])
+{
+    int rank;
+    int nProcesses;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
-	printf("Hello from %i/%i\n", rank, nProcesses);
+    int nextProc    = rank + 1;
+    int prevProc    = rank - 1;
+    int len         = N - rank;
 
-	if(rank==0) { // This code is run by a single process
-		int sorted = 0;
-		int aux;
-		int *v = (int*)malloc(sizeof(int)*N);
-		int *vQSort = (int*)malloc(sizeof(int)*N);
-		int i;
+    int* v, i;
+    
+    // This code is run by a single process
+    if (rank == 0)
+    {        
+        v           = malloc(sizeof(*v) * N);
+        int* vQSort = malloc(sizeof(*vQSort) * N);
 
-		// generate the vector v with random values
-		// DO NOT MODIFY
-		srand(42);
-		for(i = 0; i < N; i++)
-			v[i] = rand()%N;
+        // generate the vector v with random values
+        // DO NOT MODIFY
+        srand(42);
 
-		// make copy to check it against qsort
-		// DO NOT MODIFY
-		for(i = 0; i < N; i++)
-			vQSort[i] = v[i];
-		qsort(vQSort, N, sizeof(int), cmp);
+        for (i = 0; i != N; ++i)
+        {
+            v[i] = rand() % N;
+        }
 
-		// sort the vector v
-		// REPLACE THIS WITH YOUR SORT
-		while(!sorted) {
-			sorted = 1;
-			for(i = 0; i < N-1; i++) {
-				if(v[i] > v[i + 1]) {
-					aux = v[i];
-					v[i] = v[i + 1];
-					v[i + 1] = aux;
-					sorted = 0;
-				}
-			}
-		}
+        // make copy to check it against qsort
+        // DO NOT MODIFY
+        for (i = 0; i != N; ++i)
+        {
+            vQSort[i] = v[i];
+        }
+        qsort(vQSort, N, sizeof(int), cmp);
 
-		//displayVector(vQSort);
-		compareVectors(v, vQSort);
-	}
+        len = N - rank;
 
-	MPI_Finalize();
-	return 0;
+        for (i = 1; i != len; ++i)
+        {
+            if (v[0] > v[i])
+            {
+                MPI_Send(v, 1, MPI_INT, nextProc, MPI_TAG_UB, MPI_COMM_WORLD);
+                v[0] = v[i];
+            } else
+            {
+                MPI_Send(&v[i], 1, MPI_INT, nextProc, MPI_TAG_UB, MPI_COMM_WORLD);
+            }
+        }
+
+        MPI_Gather(v, 1, MPI_INT, &v[0], 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+
+        //displayVector(v);
+        compareVectors(v, vQSort);
+
+        free(vQSort);
+    } else
+    {
+        v = malloc(sizeof(*v));  // alftfel da seg fault... fmm MPI
+        int recvNum;
+        
+        MPI_Recv(
+            v,
+            1,
+            MPI_INT,
+            prevProc,
+            MPI_ANY_TAG,
+            MPI_COMM_WORLD,
+            MPI_STATUS_IGNORE
+        );
+
+        for (i = 1; i != len; ++i)
+        {
+            MPI_Recv(
+                &recvNum,
+                1,
+                MPI_INT,
+                prevProc,
+                MPI_ANY_TAG,
+                MPI_COMM_WORLD,
+                MPI_STATUS_IGNORE
+            );
+
+            if (*v > recvNum)
+            {
+                MPI_Send(v, 1, MPI_INT, nextProc, MPI_TAG_UB, MPI_COMM_WORLD);
+                *v = recvNum;
+            } else
+            {
+                MPI_Send(&recvNum, 1, MPI_INT, nextProc, MPI_TAG_UB, MPI_COMM_WORLD);
+            }
+        }
+
+        MPI_Gather(v, 1, MPI_INT, v, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+    }
+
+    free(v);
+
+    MPI_Finalize();
+
+    return 0;
 }
